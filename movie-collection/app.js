1a01f4fb1fb5e56a169715a1a62e949df1fce6e7
Vaneesh Behl <vaneesh.behl@gmail.com>
2018-12-28 14:21:05 +0530

    Committing movie-app project
    Committing movie-app project

---
+++ movie-collection/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
+var app = require('express')();
+module.exports = app; // for testing
+
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+	
+   // add swagger-ui
+   app.use(SwaggerUi(swaggerExpress.runner.swagger));
+	
+  // install middleware
+  swaggerExpress.register(app);
+
+  var port = process.env.PORT || 10010;
+  app.listen(port);
+
+  if (swaggerExpress.runner.swagger.paths['/movie']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port);
+  }
+});
